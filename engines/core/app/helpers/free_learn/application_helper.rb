70e2b462e50671bb708c38fc66707b5a4f9d638f
abenitoc <vermibeto@gmail.com>
2015-08-31 15:46:45 +0200

    Added Stuff until devise not working for some reason

---
+++ engines/core/app/helpers/free_learn/application_helper.rb
@@ -0,0 +1,15 @@
+module Core
+  module ApplicationHelper
+  
+  	FLASH_CLASSES = {
+		notice: "alert alert-info",
+		success: "alert alert-success",
+		alert: "alert alert-danger",
+		error: "alert alert-danger"
+	}
+	def flash_class(level)
+		FLASH_CLASSES[level]
+	end
+
+  end
+end
--- engines/core/config/routes.rb
+++ engines/core/config/routes.rb
@@ -1,3 +1,4 @@
 FreeLearn::Core::Engine.routes.draw do
+  	devise_for :users, class_name: "FreeLearn::User", module: :devise
 	root to: "dashboard#index"
 end
