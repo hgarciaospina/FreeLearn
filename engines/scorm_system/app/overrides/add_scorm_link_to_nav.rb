f08b6fd73a4adcaa1714853be7bd4705ff70dd6d
abenitoc <vermibeto@gmail.com>
2015-11-04 18:02:55 +0100

    Added original to deface

--- engines/scorm_system/app/overrides/add_scorm_link_to_nav.rb
+++ engines/scorm_system/app/overrides/add_scorm_link_to_nav.rb
@@ -2,4 +2,5 @@ Deface::Override.new(:virtual_path => "layouts/free_learn/application",
                      :name => "add_scorm_link_to_nav",
                      :insert_after => "[data-freelearn-hook='main_nav']",
                      :partial => "overrides/scorm_link",
-                     :namespaced => true )
+                     :namespaced => true,
+                     :original => "21839ca5b21e8c560d4eecc4ab2ccc0caec37ddb" )
