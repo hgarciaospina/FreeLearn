381348191799f00ce91d474d908ed1fa03d4e5e7
berto_abc <bertocode@gmail.com>
2016-02-23 16:50:42 +0100

    Main functionalities modularized

--- engines/scorm_system/config/routes.rb
+++ engines/scorm_system/config/routes.rb
@@ -1,5 +1,7 @@
 FreeLearn::Core::Engine.routes.draw do
-	scope module: 'scorm_system/scorm' do
-		
+	scope module: 'scorm_system' do
+		resources :scorm_file
+		resources :lo
+		get 'lo/:id/metadata' => 'lo#metadata'
 	end
 end
