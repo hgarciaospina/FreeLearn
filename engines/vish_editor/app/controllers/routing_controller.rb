90fc0baabdeb1c68c245fa60dadf8a7f9d4b5010
abenitoc <vermibeto@gmail.com>
2016-03-04 15:43:01 +0100

    Unable to initialize constant

--- engines/vish_editor/app/controllers/routing_controller.rb
+++ engines/vish_editor/app/controllers/routing_controller.rb
@@ -2,15 +2,15 @@ module FreeLearn::VishEditor
 	class RoutingController < ApplicationController
 
 		def editor
-
+			render "excursions/vish_editor"
 		end
 
 		def viewer_full
-
+			render "excursions/vish_viewer"
 		end
 
 		def viewer_scorm
-
+			render "excursions/vish_viewer_scorm"
 		end
 	
 	end
