b738bd19a84b2eea818dcd568d5c797e172cdd3e
berto_abc <bertocode@gmail.com>
2016-03-09 12:55:11 +0100

    Added scope to engine now working

--- engines/vish_editor/config/routes.rb
+++ engines/vish_editor/config/routes.rb
@@ -1,5 +1,7 @@
 FreeLearn::Core::Engine.routes.draw do
-	get "editor" => "routing#editor"
-	get "viewer" => "routing#viewer_full"
-	get "viewer_scorm" => "routing#viewer_scorm"
+	scope module: 'vish_editor' do
+		get "editor" => "routing#editor"
+		get "viewer" => "routing#viewer_full"
+		get "viewer_scorm" => "routing#viewer_scorm"
+	end
 end
