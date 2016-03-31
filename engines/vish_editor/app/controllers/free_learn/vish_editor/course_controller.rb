a99995b478a27268771d24ca8bd8c59218b824cc
berto_abc <bertocode@gmail.com>
2016-03-31 17:24:07 +0200

    Changed controllers

--- engines/vish_editor/app/controllers/free_learn/vish_editor/course_controller.rb
+++ engines/vish_editor/app/controllers/free_learn/vish_editor/course_controller.rb
@@ -1,10 +1,32 @@
 module FreeLearn
   module VishEditor
     class CourseController < FreeLearn::ApplicationController
-
-      def show
-
-      end
+        
+        def new
+            new! do |format|
+                format.html{
+                    redirect_to new_course_path(@course)
+                }
+            end
+        end
+        
+        def edit
+            edit! do |format|
+                format.html{
+                    redirect_to edit_course_path(@course)
+                }
+            end
+        end
+        
+        def show
+            show! do |format|
+                format.html{
+                    redirect_to course_path(@course)
+                }
+            end
+        end
+        
+        
         #Method to save excursions from vish_editor
     	def save_course
             json_parsed = JSON.parse(params[:excursion][:json])
--- engines/vish_editor/config/routes.rb
+++ engines/vish_editor/config/routes.rb
@@ -1,18 +1,14 @@
 FreeLearn::Core::Engine.routes.draw do
 	scope module: 'vish_editor' do
 
-
-
-    #layouts?
+    #layouts
     get "editor" => "routing#editor"
-		get "editor_full" => "routing#editor_full", :format => :full
-		get "viewer" => "routing#viewer_full"
-		get "viewer_scorm" => "routing#viewer_scorm"
+    get "editor_full" => "routing#editor_full", :format => :full
+    get "viewer" => "routing#viewer_full"
+    get "viewer_scorm" => "routing#viewer_scorm"
 
     #API
-    get "course/:id" => "course#show" #TODO
-    get "course/:id/edit" => "course#edit" #TODO
-    post "upload_course" => "course#save_course"
-
+    resources :course
+    
 	end
 end
