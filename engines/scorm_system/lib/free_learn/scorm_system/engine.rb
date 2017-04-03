55476e91cf3df6ce4108fd772881ba597c38dd68
abenitoc <bertocode@gmail.com>
2017-04-03 19:13:13 +0200

    Modify some behaviour

--- engines/scorm_system/lib/free_learn/scorm_system/engine.rb
+++ engines/scorm_system/lib/free_learn/scorm_system/engine.rb
@@ -12,6 +12,17 @@ module FreeLearn
           end
         end
       end
+        
+    config.assets.enabled = true
+      initializer "free_learn.scorm_system.assets.precompile" do |app|
+        app.config.assets.precompile += %w( application.js application.css )
+      end
+      config.local_asset_js_path = File.join(root, "app", "assets", "javascripts").to_s
+      config.local_asset_css_path = File.join(root, "app", "assets", "stylesheets").to_s
+
+      initializer "static assets" do |app|
+        app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
+      end
 
       config.to_prepare do
         Dir.glob(Engine.root.join("app", "decorators", "**", "*_decorator*.rb")) do |c|
